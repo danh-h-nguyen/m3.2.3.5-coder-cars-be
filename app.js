c0d2f33314640c0e69e0d7bab6d93d41ba7f940e
danh-h-nguyen <hoangdanh3393@gmail.com>
2025-03-03 16:29:09 +0700

    first commit 2

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Connect to MONGODB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://myAtlasDBUser:123@cluster0.h570m.mongodb.net/coder_cars?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("✅ Connected to Database!"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
